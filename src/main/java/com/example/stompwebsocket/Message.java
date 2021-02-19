f5f4824e52869555df1128f96d55ad37fe0145ca
Iulia <moscalenco.iulia@gmail.com>
2021-02-19 13:58:16 +0200

    Spring Websocket with STOMP and SockJS

---
+++ src/main/java/com/example/stompwebsocket/Message.java
@@ -0,0 +1,21 @@
+package com.example.stompwebsocket;
+
+public class Message {
+
+    private String name;
+
+    public Message(String name) {
+        this.name = name;
+    }
+
+    public Message() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
